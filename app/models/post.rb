56947b073f647679b48641a65375536e4ab0e243
Zoran Conevski <z.conevski@yahoo.com>
2016-07-18 10:08:11 +0200

    basic rails app

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+	
+
+  validates :content, :presence => true, :length => { :minimum => 20 }
+  validates :title, :presence => true
+  validates :name, :presence => true
+end
